482036cf590f83afb58725569cd1804468949ad2
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 19:29:36 +0530

    Update dev_build.sh

--- dev_build.sh
+++ dev_build.sh
@@ -3,6 +3,8 @@
 echo "building image from docker-compose-file"
 docker-compose build
 docker images
+echo "running a container"
+docker-compose up -d
 echo "pushing the image to hub"
 docker tag zen_app_dev_region_zen_app praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
