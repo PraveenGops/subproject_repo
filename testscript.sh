3101e575e8290c6365ad769d35065300e794ae73
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 16:02:13 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-
 echo $branch
+if [ "$branch" == "dev" ]; then
+echo "building image from docker-compose-file"
+docker-compose build
+docker images
+echo "pushing the image to hub"
+docker tag subproject_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
+docker login -u praveengdocker -p G@7160598
+docker push praveengdocker/dev_region:image${BUILD_NUMBER}
+elif [ "$branch" == "main" ];
+echo "building image from docker-compose-file"
+docker-compose build
+docker images
+echo "pushing the image to hub"
+docker tag subproject_zenapp praveengdocker/prod_region1:image${BUILD_NUMBER}
+docker login -u praveengdocker -p G@7160598
+docker push praveengdocker/prod_region1:image${BUILD_NUMBER}
+fi
