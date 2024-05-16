ce483a7c12616e8b3bc614619190dda0ba993c3f
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 16:50:35 +0530

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#branch=$(git rev-parse --abbrev-ref HEAD)
+#echo $branch
+if [ "$branch" == "dev_region" ]; then
+echo "building image from docker-compose-file"
+docker-compose build
+docker images
+echo "pushing the image to hub"
+docker tag zen_app_zen_app praveengdocker/dev_region:image${BUILD_NUMBER}
+docker login -u praveengdocker -p G@7160598
+docker push praveengdocker/dev_region:image${BUILD_NUMBER}
+else [ "$branch" == "main" ];
+echo "building image from docker-compose-file"
+docker-compose build
+docker images
+echo "pushing the image to hub"
+docker tag zen_app_zen_app praveengdocker/prod_region1:image${BUILD_NUMBER}
+docker login -u praveengdocker -p G@7160598
+docker push praveengdocker/prod_region1:image${BUILD_NUMBER}
+fi
