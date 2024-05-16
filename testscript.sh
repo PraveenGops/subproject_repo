94471874d9404e7ba59e9d77769880bc1b761948
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 17:00:30 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -10,12 +10,14 @@ echo "pushing the image to hub"
 docker tag zen_app_zen_app praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:image${BUILD_NUMBER}
-else [ "$branch" == "main" ];
-echo "building image from docker-compose-file"
-docker-compose build
-docker images
-echo "pushing the image to hub"
-docker tag zen_app_zen_app praveengdocker/prod_region1:image${BUILD_NUMBER}
-docker login -u praveengdocker -p G@7160598
-docker push praveengdocker/prod_region1:image${BUILD_NUMBER}
+#else [ "$branch" == "main" ];
+#echo "building image from docker-compose-file"
+#docker-compose build
+#docker images
+#echo "pushing the image to hub"
+#docker tag zen_app_zen_app praveengdocker/prod_region1:image${BUILD_NUMBER}
+#docker login -u praveengdocker -p G@7160598
+#docker push praveengdocker/prod_region1:image${BUILD_NUMBER}
+else
+  echo "no"
 fi
