658b4257576151b9cf35ac677e391038de188751
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 16:24:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ echo "pushing the image to hub"
 docker tag subproject_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:image${BUILD_NUMBER}
+#echo "hello world"
