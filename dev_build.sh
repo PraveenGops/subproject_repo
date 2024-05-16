354d6e278895170a0c3a930defdabbf738c21955
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 19:13:33 +0530

    Update dev_build.sh

--- dev_build.sh
+++ dev_build.sh
@@ -4,13 +4,6 @@ echo "building image from docker-compose-file"
 docker-compose build
 docker images
 echo "pushing the image to hub"
-<<<<<<< HEAD:build.sh
-docker tag zen_app_zen_app praveengdocker/prod1:image${BUILD_NUMBER}
-docker login -u praveengdocker -p G@7160598
-docker push praveengdocker/prod1:image${BUILD_NUMBER}
-=======
 docker tag zen_app_dev_region_zen_app praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:image${BUILD_NUMBER}
-#echo "hello world"
->>>>>>> origin/dev_region:dev_build.sh
