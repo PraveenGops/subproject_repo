8055035c37a24adde417133d93006bfa68cff0c4
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 19:30:02 +0530

    Update prod_build.sh

--- prod_build.sh
+++ prod_build.sh
@@ -3,6 +3,8 @@
 echo "building image from docker-compose-file"
 docker-compose build
 docker images
+echo "running a container"
+docker-compose up -d
 echo "pushing the image to hub"
 docker tag zen_app_prod_region_zen_app praveengdocker/prod_region1:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
