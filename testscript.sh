ebb2d5904bb68b59e16602d3efcec748862ac2ff
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 16:05:03 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -10,7 +10,7 @@ echo "pushing the image to hub"
 docker tag subproject_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:image${BUILD_NUMBER}
-elif [ "$branch" == "main" ];
+else [ "$branch" == "main" ];
 echo "building image from docker-compose-file"
 docker-compose build
 docker images
