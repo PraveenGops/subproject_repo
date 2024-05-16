8b21dd455d8a0349b7b7f31643b0f263063fc8f9
PraveenGops <gor9816@gmail.com>
2024-05-16 12:48:17 +0530

    sec_commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "building image from docker-compose-file"
+docker-compose build
+docker images
+echo "pushing the image to hub"
+docker tag subproject_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
+docker login -u praveengdocker -p G@7160598
+docker push praveengdocker/dev_region:image${BUILD_NUMBER}
